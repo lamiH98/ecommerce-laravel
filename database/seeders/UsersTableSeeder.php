56606308c9eb92e09315f7d672b36f1b0d87ed4f
lamiH98 <52789739+lamiH98@users.noreply.github.com>
2021-11-03 17:08:30 +0300

    Update UsersTableSeeder.php

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -13,6 +13,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(20)->create();
+        // \App\Models\User::factory(20)->create();
     }
 }
